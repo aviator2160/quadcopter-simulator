4559614a3f7dcf28cd0981e047f7ec646298f7e3
Avi Mittal <avim@uw.edu>
2021-07-01 17:24:56 -0700

    applied generalization to all sims

--- quad_sim.py
+++ quad_sim.py
@@ -54,45 +54,47 @@ def Multi_Point2Point():
                {'time': 10, 'position': (-1, 1,4)}]
     SIM_DURATION = 16 # simulated seconds
     # Define the quadcopters
-    QUADCOPTERS={'q1':{'position':[1,0,4],'orientation':[0,0,0],'L':0.3,'r':0.1,'prop_size':[10,4.5],'weight':1.2},
+    QUADCOPTER_DEFS={'q1':{'position':[1,0,4],'orientation':[0,0,0],'L':0.3,'r':0.1,'prop_size':[10,4.5],'weight':1.2},
         'q2':{'position':[-1,0,4],'orientation':[0,0,0],'L':0.15,'r':0.05,'prop_size':[6,4.5],'weight':0.7}}
     # Controller parameters
-    CONTROLLER_1_PARAMETERS = {'Motor_limits':[4000,9000],
-                        'Tilt_limits':[-10,10],
-                        'Yaw_Control_Limits':[-900,900],
-                        'Z_XY_offset':500,
-                        'Linear_PID':{'P':[300,300,7000],'I':[0.04,0.04,4.5],'D':[450,450,5000]},
-                        'Linear_To_Angular_Scaler':[1,1,0],
-                        'Yaw_Rate_Scaler':0.18,
-                        'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
-                        }
-    CONTROLLER_2_PARAMETERS = {'Motor_limits':[4000,9000],
-                        'Tilt_limits':[-10,10],
-                        'Yaw_Control_Limits':[-900,900],
-                        'Z_XY_offset':500,
-                        'Linear_PID':{'P':[300,300,7000],'I':[0.04,0.04,4.5],'D':[450,450,5000]},
-                        'Linear_To_Angular_Scaler':[1,1,0],
-                        'Yaw_Rate_Scaler':0.18,
-                        'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
-                        }
+    CONTROLLER_DEFS={
+        'q1':{
+            'Type':'pid_p2p',
+            'Goals':GOALS_1,
+            'Motor_limits':[4000,9000],
+            'Tilt_limits':[-10,10],
+            'Yaw_Control_Limits':[-900,900],
+            'Z_XY_offset':500,
+            'Linear_PID':{'P':[300,300,7000],'I':[0.04,0.04,4.5],'D':[450,450,5000]},
+            'Linear_To_Angular_Scaler':[1,1,0],
+            'Yaw_Rate_Scaler':0.18,
+            'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
+        }, 
+        'q2':{
+            'Type':'pid_p2p',
+            'Goals':GOALS_2,
+            'Motor_limits':[4000,9000],
+            'Tilt_limits':[-10,10],
+            'Yaw_Control_Limits':[-900,900],
+            'Z_XY_offset':500,
+            'Linear_PID':{'P':[300,300,7000],'I':[0.04,0.04,4.5],'D':[450,450,5000]},
+            'Linear_To_Angular_Scaler':[1,1,0],
+            'Yaw_Rate_Scaler':0.18,
+            'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
+        }
+    }
 
     # Catch Ctrl+C to stop threads
     signal.signal(signal.SIGINT, signal_handler)
-    # Make objects for quadcopter, gui and controllers
-    quad = quadcopter.Quadcopters(quads=QUADCOPTERS)
-    ctrl1 = controller.Controller_PID_Point2Point(quad.get_state,quad.get_time,quad.set_motor_speeds,goals=GOALS_1,params=CONTROLLER_1_PARAMETERS,quad_identifier='q1')
-    ctrl2 = controller.Controller_PID_Point2Point(quad.get_state,quad.get_time,quad.set_motor_speeds,goals=GOALS_2,params=CONTROLLER_2_PARAMETERS,quad_identifier='q2')
-    gui_object = gui.GUI(quads=QUADCOPTERS,get_data=(quad.get_position,quad.get_orientation),get_time=quad.get_time)
+    # Make objects for dynamics (quadcopters/controllers) and gui
+    dyn_object = dynamics.DynamicsManager(QUAD_DEFS=QUADCOPTER_DEFS, CTRL_DEFS=CONTROLLER_DEFS)
+    gui_object = gui.GUI(QUAD_DEFS=QUADCOPTER_DEFS, get_data=dyn_object.visual_data, get_time=dyn_object.get_time)
     # Start the threads
-    quad.start_thread(dt=QUAD_DYNAMICS_UPDATE,time_scaling=TIME_SCALING)
-    ctrl1.start_thread(dt=CONTROLLER_DYNAMICS_UPDATE)
-    ctrl2.start_thread(dt=CONTROLLER_DYNAMICS_UPDATE)
+    dyn_object.start_threads(phys_dt=PHYSICAL_DYNAMICS_UPDATE, ctrl_dt=CONTROLLER_DYNAMICS_UPDATE, time_scaling=TIME_SCALING)
     # Update the GUI while switching between destination poitions
-    gui_object.animate(duration=SIM_DURATION, pause_sim=quad.pause_thread, frame_rate=30)
+    gui_object.animate(duration=SIM_DURATION, pause_sim=dyn_object.pause_threads, frame_rate=30)
     # Stop threads once animations are done
-    quad.stop_thread()
-    ctrl1.stop_thread()
-    ctrl2.stop_thread()
+    dyn_object.stop_threads()
 
 def Single_Velocity():
     # Set goals to go to
@@ -102,36 +104,37 @@ def Single_Velocity():
              {'time': 15, 'position': (   0,-0.5,2)}]
     SIM_DURATION = 20 # simulated seconds
     # Define the quadcopters
-    QUADCOPTER={'q1':{'position':[0,0,0],'orientation':[0,0,0],'L':0.3,'r':0.1,'prop_size':[10,4.5],'weight':1.2}}
+    QUADCOPTER_DEFS={'q1':{'position':[0,0,0],'orientation':[0,0,0],'L':0.3,'r':0.1,'prop_size':[10,4.5],'weight':1.2}}
     # Controller parameters
-    CONTROLLER_PARAMETERS = {'Motor_limits':[4000,9000],
-                        'Tilt_limits':[-10,10],
-                        'Yaw_Control_Limits':[-900,900],
-                        'Z_XY_offset':500,
-                        'Linear_PID':{'P':[2000,2000,7000],'I':[0.25,0.25,4.5],'D':[50,50,5000]},
-                        'Linear_To_Angular_Scaler':[1,1,0],
-                        'Yaw_Rate_Scaler':0.18,
-                        'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
+    CONTROLLER_DEFS={'q1':{
+                            'Type':'pid_velocity',
+                            'Goals':GOALS,
+                            'Motor_limits':[4000,9000],
+                            'Tilt_limits':[-10,10],
+                            'Yaw_Control_Limits':[-900,900],
+                            'Z_XY_offset':500,
+                            'Linear_PID':{'P':[2000,2000,7000],'I':[0.25,0.25,4.5],'D':[50,50,5000]},
+                            'Linear_To_Angular_Scaler':[1,1,0],
+                            'Yaw_Rate_Scaler':0.18,
+                            'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
+                            }
                         }
 
     # Catch Ctrl+C to stop threads
     signal.signal(signal.SIGINT, signal_handler)
-    # Make objects for quadcopter, gui and controller
-    quad = quadcopter.Quadcopters(QUADCOPTER)
-    ctrl = controller.Controller_PID_Velocity(quad.get_state,quad.get_time,quad.set_motor_speeds,goals=GOALS,params=CONTROLLER_PARAMETERS,quad_identifier='q1')
-    gui_object = gui.GUI(quads=QUADCOPTER, get_data=(quad.get_position,quad.get_orientation), get_time=quad.get_time)
+    # Make objects for dynamics (quadcopters/controllers) and gui
+    dyn_object = dynamics.DynamicsManager(QUAD_DEFS=QUADCOPTER_DEFS, CTRL_DEFS=CONTROLLER_DEFS)
+    gui_object = gui.GUI(QUAD_DEFS=QUADCOPTER_DEFS, get_data=dyn_object.visual_data, get_time=dyn_object.get_time)
     # Start the threads
-    quad.start_thread(dt=QUAD_DYNAMICS_UPDATE,time_scaling=TIME_SCALING)
-    ctrl.start_thread(dt=CONTROLLER_DYNAMICS_UPDATE)
+    dyn_object.start_threads(phys_dt=PHYSICAL_DYNAMICS_UPDATE, ctrl_dt=CONTROLLER_DYNAMICS_UPDATE, time_scaling=TIME_SCALING)
     # Update the GUI while switching between destination poitions
-    gui_object.animate(duration=SIM_DURATION, pause_sim=quad.pause_thread, frame_rate=30)
+    gui_object.animate(duration=SIM_DURATION, pause_sim=dyn_object.pause_threads, frame_rate=30)
     # Stop threads once animations are done
-    quad.stop_thread()
-    ctrl.stop_thread()
+    dyn_object.stop_threads()
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Quadcopter Simulator")
-    parser.add_argument("--sim", help='single_p2p, multi_p2p or single_velocity', default='single_p2p')
+    parser.add_argument("--sim", help='single_p2p, multi_p2p or single_velocity', default='single_velocity')
     parser.add_argument("--time_scale", type=float, default=-1.0, help='Time scaling factor. 0.0:fastest,1.0:realtime,>1:slow, ex: --time_scale 0.1')
     parser.add_argument("--quad_update_time", type=float, default=0.0, help='delta time for quadcopter dynamics update(seconds), ex: --quad_update_time 0.002')
     parser.add_argument("--controller_update_time", type=float, default=0.0, help='delta time for controller update(seconds), ex: --controller_update_time 0.005')
