63726a39496ff7ea405f091f53cd867d49b7051b
Avi Mittal <avim@uw.edu>
2021-07-28 01:07:16 -0700

    deleted pid velocity controller, which did nothing physically meaningful

--- controller.py
+++ controller.py
@@ -41,7 +41,7 @@ class Controller_PID_Point2Point():
     def wrap_angle(self,val):
         return( ( val + np.pi) % (2 * np.pi ) - np.pi )
 
-    def update(self, wut):
+    def update(self, dt):
         if (len(self.goals) > 0) and (self.get_time() > self.goals[0]['time']):
             self.update_goal(self.goals.pop(0))
         [dest_x,dest_y,dest_z] = self.curr_goal['position']
@@ -49,9 +49,9 @@ class Controller_PID_Point2Point():
         x_error = dest_x-x
         y_error = dest_y-y
         z_error = dest_z-z
-        self.xi_term += self.LINEAR_I[0]*x_error
-        self.yi_term += self.LINEAR_I[1]*y_error
-        self.zi_term += self.LINEAR_I[2]*z_error
+        self.xi_term += self.LINEAR_I[0]*x_error*dt
+        self.yi_term += self.LINEAR_I[1]*y_error*dt
+        self.zi_term += self.LINEAR_I[2]*z_error*dt
         dest_x_dot = self.LINEAR_P[0]*(x_error) + self.LINEAR_D[0]*(-x_dot) + self.xi_term
         dest_y_dot = self.LINEAR_P[1]*(y_error) + self.LINEAR_D[1]*(-y_dot) + self.yi_term
         dest_z_dot = self.LINEAR_P[2]*(z_error) + self.LINEAR_D[2]*(-z_dot) + self.zi_term
@@ -85,43 +85,6 @@ class Controller_PID_Point2Point():
 
     def update_yaw_target(self,target):
         self.yaw_target = self.wrap_angle(target)
-
-class Controller_PID_Velocity(Controller_PID_Point2Point):
-    def update(self):
-        if (len(self.goals) > 0) and (self.get_time() > self.goals[0]['time']):
-            self.update_goal(self.goals.pop(0))
-        [dest_x,dest_y,dest_z] = self.curr_goal['position']
-        [x,y,z,x_dot,y_dot,z_dot,theta,phi,gamma,theta_dot,phi_dot,gamma_dot] = self.get_state()
-        x_error = dest_x-x_dot
-        y_error = dest_y-y_dot
-        z_error = dest_z-z
-        self.xi_term += self.LINEAR_I[0]*x_error
-        self.yi_term += self.LINEAR_I[1]*y_error
-        self.zi_term += self.LINEAR_I[2]*z_error
-        dest_x_dot = self.LINEAR_P[0]*(x_error) + self.LINEAR_D[0]*(-x_dot) + self.xi_term
-        dest_y_dot = self.LINEAR_P[1]*(y_error) + self.LINEAR_D[1]*(-y_dot) + self.yi_term
-        dest_z_dot = self.LINEAR_P[2]*(z_error) + self.LINEAR_D[2]*(-z_dot) + self.zi_term
-        throttle = np.clip(dest_z_dot,self.Z_LIMITS[0],self.Z_LIMITS[1])
-        dest_theta = self.LINEAR_TO_ANGULAR_SCALER[0]*(dest_x_dot*np.sin(gamma)-dest_y_dot*np.cos(gamma))
-        dest_phi = self.LINEAR_TO_ANGULAR_SCALER[1]*(dest_x_dot*np.cos(gamma)+dest_y_dot*np.sin(gamma))
-        dest_gamma = self.curr_goal['yaw']
-        dest_theta,dest_phi = np.clip(dest_theta,self.TILT_LIMITS[0],self.TILT_LIMITS[1]),np.clip(dest_phi,self.TILT_LIMITS[0],self.TILT_LIMITS[1])
-        theta_error = dest_theta-theta
-        phi_error = dest_phi-phi
-        gamma_dot_error = (self.YAW_RATE_SCALER*self.wrap_angle(dest_gamma-gamma)) - gamma_dot
-        self.thetai_term += self.ANGULAR_I[0]*theta_error
-        self.phii_term += self.ANGULAR_I[1]*phi_error
-        self.gammai_term += self.ANGULAR_I[2]*gamma_dot_error
-        x_val = self.ANGULAR_P[0]*(theta_error) + self.ANGULAR_D[0]*(-theta_dot) + self.thetai_term
-        y_val = self.ANGULAR_P[1]*(phi_error) + self.ANGULAR_D[1]*(-phi_dot) + self.phii_term
-        z_val = self.ANGULAR_P[2]*(gamma_dot_error) + self.gammai_term
-        z_val = np.clip(z_val,self.YAW_CONTROL_LIMITS[0],self.YAW_CONTROL_LIMITS[1])
-        m1 = throttle + x_val + z_val
-        m2 = throttle + y_val - z_val
-        m3 = throttle - x_val + z_val
-        m4 = throttle - y_val - z_val
-        M = np.clip([m1,m2,m3,m4],self.MOTOR_LIMITS[0],self.MOTOR_LIMITS[1])
-        self.actuate_motors(M)
 
 class ControllerTypeNotFoundError(Exception):
     pass
