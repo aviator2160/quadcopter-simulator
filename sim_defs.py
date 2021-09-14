668b070ece3fbb3708fe3008d58ae85d8f2575ec
Avi Mittal <avim@uw.edu>
2021-09-13 19:35:46 -0700

    matched parameters of sims with slung loads to those without, for easier comparison

--- sim_defs.py
+++ sim_defs.py
@@ -36,6 +36,36 @@ Multi_Slung_LQR_P2P = {
         },
     }
 
+Multi_Slung_LQR_P2P = {
+    'SIM_DURATION': 8, # simulated seconds
+    # Define the quadcopters
+    'QUADCOPTER_DEFS':{
+        'q1':{'position':[-1, 1,4],},
+        'q2':{'position':[-3, 1,4],},
+        'q3':{'position':[-3,-1,4],},
+        'q4':{'position':[-1,-1,4],},
+        },
+    'PAYLOAD_DEFS':{
+        'p1':{
+            'position':[-2,0,2],'orientation':[0,0,0],'x': 0.4,'y': 0.4,'z': 0.2,'mass': 4.0,
+            'hardpoints':[[0.2,0.2,0],[-0.2,0.2,0],[-0.2,-0.2,0],[0.2,-0.2,0]]
+            },
+        },
+    # Controller parameters
+    'CONTROLLER_DEFS':{
+        'q1':{'Type':'lqr_p2p','Goals':[{'time': 0,'position':(2, 1,5)}],},
+        'q2':{'Type':'lqr_p2p','Goals':[{'time': 0,'position':(0, 1,5)}],},
+        'q3':{'Type':'lqr_p2p','Goals':[{'time': 0,'position':(0,-1,5)}],},
+        'q4':{'Type':'lqr_p2p','Goals':[{'time': 0,'position':(2,-1,5)}],},
+        },
+    'CABLE_DEFS':{
+        'c1':{'quad':'q1','load':'p1','hardpoint': 0,'stiffness': 100,'damping': 1},
+        'c2':{'quad':'q2','load':'p1','hardpoint': 1,'stiffness': 100,'damping': 1},
+        'c3':{'quad':'q3','load':'p1','hardpoint': 2,'stiffness': 100,'damping': 1},
+        'c4':{'quad':'q4','load':'p1','hardpoint': 3,'stiffness': 100,'damping': 1},
+        },
+    }
+
 Multi_Slung_PID_P2P = {
     'SIM_DURATION': 8, # simulated seconds
     # Define the quadcopters
@@ -66,15 +96,69 @@ Multi_Slung_PID_P2P = {
         },
     }
 
+Slung_Both_P2P = {
+    'SIM_DURATION': 32, # simulated seconds
+    # Define the quadcopters
+    'QUADCOPTER_DEFS':{
+        'q1':{'position':[0, 0.5,4],},
+        'q2':{'position':[0,-0.5,4],},
+        },
+    'PAYLOAD_DEFS':{
+        'p1':{
+            'position':[0,0.5,2],'orientation':[0,0,0],'x': 0.4,'y': 0.4,'z': 0.2,'mass': 1.0,
+            'hardpoints':[[0.3,0,0],[0,0.3,0],[-0.3,0,0],[0,-0.3,0]]
+            },
+        'p2':{
+            'position':[0,-0.5,2],'orientation':[0,0,0],'x': 0.4,'y': 0.4,'z': 0.2,'mass': 1.0,
+            'hardpoints':[[0.3,0,0],[0,0.3,0],[-0.3,0,0],[0,-0.3,0]]
+            },
+        },
+    # Controller parameters
+    'CONTROLLER_DEFS':{
+        'q1':{
+            'Type':'lqr_p2p',
+            'Goals':[
+                {'time': 0,  'position': ( 1, 1.5,2), 'yaw': 0},
+                {'time': 8,  'position': ( 1,-0.5,4), 'yaw': 3.14},
+                {'time': 16, 'position': (-1,-0.5,2), 'yaw': -1.54},
+                {'time': 24, 'position': (-1, 1.5,4), 'yaw': 1.54}
+                ],
+            'Q': np.diag([2,2,250, 5,5,20, 0.1,0.1,0.1, 1,1,0.1]),
+            },
+        'q2':{
+            'Type':'pid_p2p',
+            'Goals':[
+                {'time': 0,  'position': ( 1, 0.5,2), 'yaw': 0},
+                {'time': 8,  'position': ( 1,-1.5,4), 'yaw': 3.14},
+                {'time': 16, 'position': (-1,-1.5,2), 'yaw': -1.54},
+                {'time': 24, 'position': (-1, 0.5,4), 'yaw': 1.54}
+                ],
+            'Linear_PID':{'P':[1,1,5],'I':[0.01,0.01,0.3],'D':[2.5,2.5,2.5]},
+            'Angular_PID':{'P':[15,15,8],'I':[0.01,0.1,0.1],'D':[4,4,15]},
+            },
+        },
+    'CABLE_DEFS':{
+        'c11':{'quad':'q1','load':'p1','hardpoint': 0,'stiffness': 100,'damping': 1},
+        'c12':{'quad':'q1','load':'p1','hardpoint': 1,'stiffness': 100,'damping': 1},
+        'c13':{'quad':'q1','load':'p1','hardpoint': 2,'stiffness': 100,'damping': 1},
+        'c14':{'quad':'q1','load':'p1','hardpoint': 3,'stiffness': 100,'damping': 1},
+        'c21':{'quad':'q2','load':'p2','hardpoint': 0,'stiffness': 100,'damping': 1},
+        'c22':{'quad':'q2','load':'p2','hardpoint': 1,'stiffness': 100,'damping': 1},
+        'c23':{'quad':'q2','load':'p2','hardpoint': 2,'stiffness': 100,'damping': 1},
+        'c24':{'quad':'q2','load':'p2','hardpoint': 3,'stiffness': 100,'damping': 1},
+        },
+    }
+
+
 Slung_LQR_P2P = {
     'SIM_DURATION': 32, # simulated seconds
     # Define the quadcopters
     'QUADCOPTER_DEFS':{
-        'q1':{'position':[-1,0,4],},
+        'q1':{'position':[0,0,4],},
         },
     'PAYLOAD_DEFS':{
         'p1':{
-            'position':[-1,0,2],'orientation':[0,0,0],'x': 0.4,'y': 0.4,'z': 0.2,'mass': 1.0,
+            'position':[0,0,2],'orientation':[0,0,0],'x': 0.4,'y': 0.4,'z': 0.2,'mass': 1.0,
             'hardpoints':[[0.3,0,0],[0,0.3,0],[-0.3,0,0],[0,-0.3,0]]
             },
         },
@@ -103,11 +187,11 @@ Slung_PID_P2P = {
     'SIM_DURATION': 32, # simulated seconds
     # Define the quadcopters
     'QUADCOPTER_DEFS':{
-        'q1':{'position':[-1,0,4],},
+        'q1':{'position':[0,0,4],},
         },
     'PAYLOAD_DEFS':{
         'p1':{
-            'position':[-1,0,2],'orientation':[0,0,0],'x': 0.4,'y': 0.4,'z': 0.2,'mass': 1.0,
+            'position':[0,0,2],'orientation':[0,0,0],'x': 0.4,'y': 0.4,'z': 0.2,'mass': 1.0,
             'hardpoints':[[0.3,0,0],[0,0.3,0],[-0.3,0,0],[0,-0.3,0]]
             },
         },
@@ -117,12 +201,12 @@ Slung_PID_P2P = {
             'Type':'pid_p2p',
             'Goals':[
                 {'time': 0,  'position': ( 1, 1,2), 'yaw': 0},
-                {'time': 8,  'position': ( 1,-1,4), 'yaw': 0},
-                {'time': 16, 'position': (-1,-1,2), 'yaw': 0},
-                {'time': 24, 'position': (-1, 1,4), 'yaw': 0}
+                {'time': 8,  'position': ( 1,-1,4), 'yaw': 3.14},
+                {'time': 16, 'position': (-1,-1,2), 'yaw': -1.54},
+                {'time': 24, 'position': (-1, 1,4), 'yaw': 1.54}
                 ],
-            'Linear_PID':{'P':[1,1,3],'I':[0.01,0.01,0.3],'D':[1.8,1.8,1.2]},
-            'Angular_PID':{'P':[15,15,8],'I':[0.01,0.01,0.01],'D':[2,2,15]},
+            'Linear_PID':{'P':[1,1,5],'I':[0.01,0.01,0.3],'D':[1.8,1.8,2.0]},
+            'Angular_PID':{'P':[15,15,8],'I':[0.1,0.1,0.1],'D':[4,4,15]},
             },
         },
     'CABLE_DEFS':{
@@ -201,7 +285,7 @@ Multi_PID_P2P = {
                 {'time': 8, 'position': ( 1, 1,2), 'yaw': 3.14}
                 ],
             'Linear_PID':{'P':[1,1,5],'I':[0.01,0.01,0.03],'D':[1.8,1.8,2.0]},
-            'Angular_PID':{'P':[35,35,8],'I':[0.01,0.01,0.01],'D':[10,10,20]},
+            'Angular_PID':{'P':[35,35,8],'I':[0.1,0.1,0.1],'D':[10,10,20]},
             }, 
         'q2':{
             'Type':'pid_p2p',
@@ -210,7 +294,7 @@ Multi_PID_P2P = {
                 {'time': 10, 'position': (-1, 1,4), 'yaw': 3.14}
                 ],
             'Linear_PID':{'P':[1,1,3],'I':[0.01,0.01,0.03],'D':[1.8,1.8,1.2]},
-            'Angular_PID':{'P':[15,15,8],'I':[0.01,0.01,0.01],'D':[2,2,15]},
+            'Angular_PID':{'P':[15,15,8],'I':[0.1,0.1,0.1],'D':[2,2,15]},
             },
         },
     }
@@ -232,8 +316,8 @@ Both_P2P = {
                 {'time': 16, 'position': (-1,-0.5,2), 'yaw': -1.54},
                 {'time': 24, 'position': (-1, 1.5,4), 'yaw': 1.54}
                 ],
-            'Q': np.diag([1,1,50, 1,1,5, 10,10,0.1, 1,1,0.1]),
-            'R': np.diag([0.1, 0.1,0.1,0.1]),
+            'Q': np.diag([2,2,30, 1,1,5, 10,10,0.1, 1,1,0.1]),
+            'R': np.diag([0.1, 0.1,0.1,0.2]),
             'offset_gravity': 1, # Fraction of quad weight
             },
         'q2':{
@@ -244,8 +328,8 @@ Both_P2P = {
                 {'time': 16, 'position': (-1,-1.5,2), 'yaw': -1.54},
                 {'time': 24, 'position': (-1, 0.5,4), 'yaw': 1.54}
                 ],
-            'Linear_PID':{'P':[1,1,3],'I':[0.01,0.01,0.1],'D':[1.8,1.8,1.2]},
-            'Angular_PID':{'P':[15,15,8],'I':[0.01,0.01,0.01],'D':[2,2,15]},
+            'Linear_PID':{'P':[1,1,3],'I':[0.1,0.1,0.3],'D':[1.8,1.8,1.2]},
+            'Angular_PID':{'P':[15,15,8],'I':[1,1,0.1],'D':[2,2,15]},
             'offset_gravity': 1, # Fraction of quad weight
             },
         },
@@ -310,8 +394,8 @@ DEFAULT_QUAD = {
 DEFAULT_CONTROLLERS = {
     'pid_p2p':{
         'Motor_limits':[-10,10],
-        'Linear_PID':{'P':[1,1,3],'I':[0.01,0.01,0.03],'D':[1.8,1.8,1.2]},
-        'Angular_PID':{'P':[15,15,8],'I':[0.01,0.01,0.01],'D':[2,2,15]},
+        'Linear_PID':{'P':[1,1,3],'I':[0.1,0.1,0.3],'D':[1.8,1.8,1.2]},
+        'Angular_PID':{'P':[15,15,8],'I':[1,1,1],'D':[2,2,15]},
         'Linear_To_Angular_Scaler':[0.1,0.1,0],
         'offset_gravity': 1,
         },
@@ -332,6 +416,7 @@ defs = {
     'single_lqr_p2p': Single_LQR_P2P,
     'single_pid_p2p': Single_PID_P2P,
     'both_p2p': Both_P2P,
+    'slung_both_p2p': Slung_Both_P2P,
     }
 
 for sim in defs.values():
