700400e7e03ce0e29c7b633370ee3fdd47a10be8
Avi Mittal <avim@uw.edu>
2022-01-18 20:14:50 -0800

    Added documentation for util.py

--- util.py
+++ util.py
@@ -1,35 +1,52 @@
 # -*- coding: utf-8 -*-
-"""
+"""General simulation utility functions.
+
 Created on Wed Jul  7 16:52:43 2021
 
 @author: aviat
-
-General simulation utility functions
 """
+
 import numpy as np
 
-def yaw_matrix(gamma):
-    cg = np.cos(gamma)
-    sg = np.sin(gamma)
-    return np.array([[cg,-sg,0],
-                     [sg, cg,0],
-                     [ 0,  0,1]])
-
-def pitch_matrix(phi):
-    cp = np.cos(phi)
-    sp = np.sin(phi)
-    return np.array([[ cp,0,sp],
-                     [  0,1, 0],
-                     [-sp,0,cp]])
-
 def roll_matrix(theta):
+    """Outputs the 3x3 matrix of an x-axis rotation by angle theta (radians)."""
     ct = np.cos(theta)
     st = np.sin(theta)
     return np.array([[1, 0,  0],
                      [0,ct,-st],
                      [0,st, ct]])
 
+
+def pitch_matrix(phi):
+    """Outputs the 3x3 matrix of a y-axis rotation by angle phi (radians)."""
+    cp = np.cos(phi)
+    sp = np.sin(phi)
+    return np.array([[ cp,0,sp],
+                     [  0,1, 0],
+                     [-sp,0,cp]])
+
+
+def yaw_matrix(gamma):
+    """Outputs the 3x3 matrix of a z-axis rotation by angle gamma (radians)."""
+    cg = np.cos(gamma)
+    sg = np.sin(gamma)
+    return np.array([[cg,-sg,0],
+                     [sg, cg,0],
+                     [ 0,  0,1]])
+
+
 def rotation_matrix(angles):
+    """Outputs the 3x3 rotation matrix for the given angles.
+    
+    Outputs the 3x3 rotation matrix for the given list of three
+    Tait-Bryan angles: roll (x-axis), pitch (y-axis), yaw (z-axis).
+
+    Args:
+        angles: A tuple of the three Tait-Bryan angles defining the rotation:
+            0:  Roll (x-axis) angle in radians
+            1: Pitch (y-axis) angle in radians
+            2:   Yaw (z-axis) angle in radians
+    """
     R_x = roll_matrix(angles[0])
     R_y = pitch_matrix(angles[1])
     R_z = yaw_matrix(angles[2])
